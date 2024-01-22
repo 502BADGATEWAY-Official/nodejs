4430718acf9709a95bdc6c4edc5e830e6476ed98
502 Bad Gateway <128957490+502BADGATEWAY-Official@users.noreply.github.com>
2024-01-22 18:33:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# 设置各变量，WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+WEB_USERNAME=${WEB_USERNAME:-'admin'}
+WEB_PASSWORD=${WEB_PASSWORD:-'password'}
+
 # 哪吒4个参数，ssl/tls 看是否需要，不需要的话可以留空，删除或在这4行最前面加 # 以注释
 NEZHA_SERVER="$NEZHA_SERVER"
 NEZHA_PORT="$NEZHA_PORT"
@@ -10,8 +14,9 @@ NEZHA_TLS="$NEZHA_TLS"
 ARGO_AUTH=''
 ARGO_DOMAIN="$ARGO_DOMAIN"
 
-echo "ARGO_AUTH: $ARGO_AUTH"
-echo "ARGO_DOMAIN: $ARGO_DOMAIN"
+# ttyd / filebrowser argo 域名
+SSH_DOMAIN="$SSH_AUTH"
+FTP_DOMAIN="$FTP_AUTH"
 
 # 安装系统依赖
 check_dependencies() {
@@ -21,12 +26,48 @@ check_dependencies() {
   [ -n "$DEPS" ] && { apt-get update >/dev/null 2>&1; apt-get install -y $DEPS >/dev/null 2>&1; }
 }
 
+generate_alist() {
+  cat > alist.sh << EOF
+#!/usr/bin/env bash
+
+# 哪吒的4个参数
+NEZHA_SERVER="$NEZHA_SERVER"
+NEZHA_PORT="$NEZHA_PORT"
+NEZHA_KEY="$NEZHA_KEY"
+NEZHA_TLS="$NEZHA_TLS"
+
+
+# 下载最新版本 alist
+download_alist() {
+  if [ ! -e alist ]; then
+    URL=\$(wget -qO- -4 "https://api.github.com/repos/alist-org/alist/releases/latest" | grep -o "https.*alist-linux-musl-amd64.tar.gz")
+    URL=\${URL:-https://github.com/alist-org/alist/releases/download/v3.30.0/alist-linux-musl-amd64.tar.gz}
+    wget -t 2 -T 10 -N \${URL}
+    tar -zxvf alist-linux-musl-amd64.tar.gz && rm -f alist-linux-musl-amd64.tar.gz
+  fi
+}
+
+# 运行客户端
+run() {
+  [[ ! \$PROCESS =~ alist && -e alist ]] && ./alist server 2>&1 &
+}
+
+download_alist
+run
+EOF
+}
+
+
+
+
 generate_argo() {
   cat > argo.sh << ABC
 #!/usr/bin/env bash
 
 ARGO_AUTH=${ARGO_AUTH}
 ARGO_DOMAIN=${ARGO_DOMAIN}
+SSH_DOMAIN=${SSH_DOMAIN}
+FTP_DOMAIN=${FTP_DOMAIN}
 
 # 下载并运行 Argo
 check_file() {
@@ -45,6 +86,14 @@ protocol: http2
 ingress:
   - hostname: \$ARGO_DOMAIN
     service: http://localhost:5244
+EOF
+      [ -n "\${SSH_DOMAIN}" ] && cat >> tunnel.yml << EOF
+  - hostname: \$SSH_DOMAIN
+    service: http://localhost:2222
+EOF
+      [ -n "\${FTP_DOMAIN}" ] && cat >> tunnel.yml << EOF
+  - hostname: \$FTP_DOMAIN
+    service: http://localhost:3333
 EOF
       cat >> tunnel.yml << EOF
   - service: http_status:404
@@ -109,8 +158,99 @@ run
 EOF
 }
 
+generate_ttyd() {
+  cat > ttyd.sh << EOF
+#!/usr/bin/env bash
+
+# ttyd 三个参数
+WEB_USERNAME=${WEB_USERNAME}
+WEB_PASSWORD=${WEB_PASSWORD}
+SSH_DOMAIN=${SSH_DOMAIN}
+
+# 检测是否已运行
+check_run() {
+  [[ \$(pgrep -lafx ttyd) ]] && echo "ttyd 正在运行中" && exit
+}
+
+# ssh argo 域名不设置，则不安装 ttyd 服务端
+check_variable() {
+  [ -z "\${SSH_DOMAIN}" ] && exit
+}
+
+# 下载最新版本 ttyd
+download_ttyd() {
+  if [ ! -e ttyd ]; then
+    URL=\$(wget -qO- "https://api.github.com/repos/tsl0922/ttyd/releases/latest" | grep -o "https.*x86_64")
+    URL=\${URL:-https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64}
+    wget -O ttyd \${URL}
+    chmod +x ttyd
+  fi
+}
+
+# 运行 ttyd 服务端
+run() {
+  [ -e ttyd ] && nohup ./ttyd -c \${WEB_USERNAME}:\${WEB_PASSWORD} -p 2222 bash >/dev/null 2>&1 &
+}
+
+check_run
+check_variable
+download_ttyd
+run
+EOF
+}
+
+generate_filebrowser () {
+  cat > filebrowser.sh << EOF
+#!/usr/bin/env bash
+
+# filebrowser 三个参数
+WEB_USERNAME=${WEB_USERNAME}
+WEB_PASSWORD=${WEB_PASSWORD}
+FTP_DOMAIN=${FTP_DOMAIN}
+
+# 检测是否已运行
+check_run() {
+  [[ \$(pgrep -lafx filebrowser) ]] && echo "filebrowser 正在运行中" && exit
+}
+
+# 若 ftp argo 域名不设置，则不安装 filebrowser
+check_variable() {
+  [ -z "\${FTP_DOMAIN}" ] && exit
+}
+
+# 下载最新版本 filebrowser
+download_filebrowser() {
+  if [ ! -e filebrowser ]; then
+    URL=\$(wget -qO- "https://api.github.com/repos/filebrowser/filebrowser/releases/latest" | grep -o "https.*linux-amd64.*gz")
+    URL=\${URL:-https://github.com/filebrowser/filebrowser/releases/download/v2.23.0/linux-amd64-filebrowser.tar.gz}
+    wget -O filebrowser.tar.gz \${URL}
+    tar xzvf filebrowser.tar.gz filebrowser
+    rm -f filebrowser.tar.gz
+    chmod +x filebrowser
+  fi
+}
+
+# 运行 filebrowser 服务端
+run() {
+  PASSWORD_HASH=\$(./filebrowser hash \$WEB_PASSWORD)
+  [ -e filebrowser ] && nohup ./filebrowser --port 3333 --username \${WEB_USERNAME} --password "\${PASSWORD_HASH}" >/dev/null 2>&1 &
+}
+
+check_run
+check_variable
+download_filebrowser
+run
+EOF
+}
+
+generate_alist
 generate_argo
 generate_nezha
+generate_ttyd
+generate_filebrowser
 
+[ -e alist.sh ] && bash alist.sh
 [ -e nezha.sh ] && bash nezha.sh
 [ -e argo.sh ] && bash argo.sh
+[ -e ttyd.sh ] && bash ttyd.sh
+[ -e filebrowser.sh ] && bash filebrowser.sh
